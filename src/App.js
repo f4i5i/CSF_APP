e70371b74651053374faec55df2967bbeb371153
f4i5i <faisalnazir22@gmail.com>
2025-12-07 02:29:27 +0500

    Design Fixes

--- src/App.js
+++ src/App.js
@@ -181,12 +181,7 @@ return (
 } 
 /> 
 
-<Route path="/checkout" element={
-  <ProtectedRoute>
-    <Checkout />
- </ProtectedRoute> 
-} 
-/> 
+<Route path="/checkout" element={<Checkout />} /> 
 
 <Route path="/financials" element={
   <ProtectedRoute>
